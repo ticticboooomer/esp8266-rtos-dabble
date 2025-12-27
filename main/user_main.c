dab8169d9deb973a303cdf4222f2a999681086e6
ticticboooomer <115849106+ticticboooomer@users.noreply.github.com>
2025-12-27 19:36:17 +0000

    Make gpio btn click work noice

--- main/user_main.c
+++ main/user_main.c
@@ -1,33 +1,45 @@
 #include <esp_log.h>
+#include <FreeRTOS.h>
+#include <queue.h>
+#include <task.h>
 
 #include "driver/gpio.h"
-
-
 
 static const char *TAG = "main";
 
 #define GPIO_OUTPUT_LED 2
-#define GPIO_OUTPUT_PIN_SEL (1<<GPIO_OUTPUT_LED)
-#define GPIO_INPUT_BTN 16
-#define GPIO_INPUT_PIN_SEL (1<<GPIO_INPUT_BTN)
+#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_OUTPUT_LED)
+#define GPIO_INPUT_BTN 5
+#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_BTN)
 
-static void gpio_isr_handler(void* arg) {
-    const uint32_t gpio_num = (uint32_t)arg;
-    const int level = gpio_get_level(gpio_num);
-    if (level) {
-        gpio_set_level(GPIO_OUTPUT_LED, 1);
+static xQueueHandle gpio_evt_queue;
+
+static void gpio_isr_handler(void *arg) {
+    const uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task(void *arg) {
+    uint32_t io_num;
+
+    for (;;) {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            int level = gpio_get_level(io_num);
+            if (level) {
+                gpio_set_level(GPIO_OUTPUT_LED, 1);
+            } else {
+                gpio_set_level(GPIO_OUTPUT_LED, 0);
+            }
+        }
     }
-
-    ESP_LOGI(TAG, "GPIO 0 isr triggered");
 }
 
 void app_main(void) {
-
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    io_conf.pull_down_en = 1;
+    io_conf.pull_down_en = 0;
     io_conf.pull_up_en = 0;
 
     gpio_config(&io_conf);
@@ -35,15 +47,22 @@ void app_main(void) {
     io_conf.intr_type = GPIO_INTR_ANYEDGE;
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
     io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = 1;
 
     gpio_config(&io_conf);
 
+    gpio_set_level(GPIO_OUTPUT_LED, 0);
+
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    xTaskCreate(gpio_task, "gpio_task", 2048, NULL, 10, NULL);
+
     gpio_install_isr_service(0);
 
-    gpio_isr_handler_add(GPIO_INPUT_BTN, gpio_isr_handler, (void*)GPIO_INPUT_BTN);
+    gpio_isr_handler_add(GPIO_INPUT_BTN, gpio_isr_handler, (void *) GPIO_INPUT_BTN);
 
     ESP_LOGI(TAG, "Setup");
 
     while (1) {
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
